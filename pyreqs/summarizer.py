877425398ef247a2b6921db581a4472808251252
Md. Khatami <67198296+khatamirock@users.noreply.github.com>
2022-11-18 12:04:25 +0600

    Update summarizer.py

--- pyreqs/summarizer.py
+++ pyreqs/summarizer.py
@@ -1,96 +1,55 @@
+
+from pyreqs.summarizer import *
+
+from json_extract import GetValue2
 import re
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-import random
+regex = re.compile(r'<[^>]+>')
 
 
-sent = ''
+class objecth:
+    def __init__(self, objs):
+        self.title = objs['story']['headline']
+        #
+        try:
+            self.image = objs['story']['alternative']['home']['default']['hero-image']['hero-image-url']
+        except:
+            self.image = 'ERROR'
+        # news having multiple ['story-elements'] objects;;;;
+        self.news = self.newsmake(objs['story']['cards']).replace(
+            '\'', '').replace(']', '').replace('[', '')
+        self.newsurl = objs['story']['url']
+        self.rawlen
+        self.sumlen
+        self.percent
+
+        # self.image = "https://gumlet.assettype.com/" + \
+        #     objs['story']['hero-image-s3-key']
+    def newsmake(self, cards):
+        news = ''
+        for card in cards:
+            getobj = GetValue2(card)
+            # print('\n\n\n', cards)
+            news += str(getobj.get_values("text", deep=True))
+            # print(xs)
+        DOCUMENT = cleanText(news)
+        rawlen = sum(len(nw.split()) for nw in news)
+
+        similarity_matrix = getSimmat(DOCUMENT)
+
+        scores = run_page_rank(similarity_matrix)
+        news = get_top_sentences(scores, DOCUMENT, int(3))
+        sumlen = sum(len(nw.split()) for nw in news)
+        self.rawlen = rawlen
+        self.sumlen = sumlen
+        self.percent = int(((rawlen-sumlen)/rawlen)*100)
+
+        return regex.sub('', news)
 
 
-def cleanText(text):
+def newsmaker(lsts):
+    newsObjs = []
+    for news in lsts:
+        # print(news['story']['headline'])
+        newsObjs.append(objecth(news))
 
-    text = re.sub(r'\n|\r', ' ', text)
-    text = re.sub(r' +', ' ', text)
-    text = text.strip()
-    sent = text.split('।')
-    sent2 = text.split('.')
-
-    if len(sent) < len(sent2):
-        sent = sent2[:-1]
-
-    return sent
-
-
-def tokner(str):
-    return str.split()
-
-
-def getSimmat(sent):
-    vectorizer = TfidfVectorizer(tokenizer=tokner)
-    vectors = vectorizer.fit_transform(sent)
-    dt_matrix = vectors.toarray()
-
-    similarity_matrix = np.matmul(dt_matrix, dt_matrix.T)
-    return similarity_matrix
-
-
-def run_page_rank(similarity_matrix):
-
-    # constants
-    damping = 0.85  # damping coefficient, usually is .85
-    min_diff = 1e-5  # convergence threshold
-    steps = 100  # iteration steps
-
-    pr_vector = np.array([1] * len(similarity_matrix))
-
-    # Iteration
-    previous_pr = 0
-    for epoch in range(steps):
-        pr_vector = (1 - damping) + damping * \
-            np.matmul(similarity_matrix, pr_vector)
-        # print(pr_vector)
-        if abs(previous_pr - sum(pr_vector)) < min_diff:
-            break
-        else:
-            previous_pr = sum(pr_vector)
-
-    return pr_vector
-
-
-def get_top_sentences(pr_vector, sentences, number):
-
-    top_sentences = ''
-
-    if pr_vector is not None:
-
-        sorted_pr = np.argsort(pr_vector)
-        # print(sorted_pr)
-        sorted_pr = list(sorted_pr)
-        # it means from big to small... the upper thing was for small to big >>  ascending...............
-        sorted_pr.reverse()
-        # print(sorted_pr)
-        sorted_pr = sorted_pr[:10]
-        # print(sorted_pr)
-        index = 0
-        sorted_pr.sort()
-        # print(sorted_pr)
-        left = number
-        for epoch in range(number//2+2):
-            sent = sentences[sorted_pr[index]]
-            # sent = normalize_whitespace(sent)
-            top_sentences += sent+' । '
-            if index % 2 == 0:
-                top_sentences += '\n'
-            index += 1
-#       print(top_sentences)
-        sorted_pr = sorted_pr[left+1:]
-        random.shuffle(sorted_pr)
-
-        for epoch in range(left):
-            sent = sentences[sorted_pr[epoch]]
-            # sent = normalize_whitespace(sent)
-            top_sentences += sent+' । '
-            if index % 2 == 0:
-                top_sentences += '\n'
-            index += 1
-    return top_sentences
+    return newsObjs
